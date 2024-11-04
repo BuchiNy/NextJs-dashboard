625a2dabe4f85cf0dc37358ce8f87aedc83166d3
Mabuchi Nyirenda <79569415+BuchiNy@users.noreply.github.com>
2024-11-04 11:45:42 +0200

    fix 7

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,13 +2,9 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
-
-interface PageProps{
-  params: Promise<{ id: string }>;
-}
-
-export default async function Page({params}: PageProps) {
-  const {id} = await params;
+ 
+export default async function Page({params}: {params: {id: string}}) {
+  const id= params.id;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
