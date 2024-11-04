3e31f5d825cb8103896b5047820d83e18714ea27
Mabuchi Nyirenda <79569415+BuchiNy@users.noreply.github.com>
2024-11-04 11:07:21 +0200

    fixed PagePros problem

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,9 +2,13 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
- 
-export default async function Page({params}: {params: {id: string}}) {
-  const id= params.id;
+
+interface PageProps{
+  params: {id: string};
+}
+
+export default async function Page({params}: PageProps) {
+  const {id} = await params;
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
