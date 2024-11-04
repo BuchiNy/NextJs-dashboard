d2699a1c105900fa4fabad3c44f9ce0d919d25e7
Mabuchi Nyirenda <79569415+BuchiNy@users.noreply.github.com>
2024-11-04 17:02:23 +0200

    fix 8

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -4,14 +4,15 @@ import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
  
 export default async function Page({params}: {params: {id: string}}) {
-  const id= params.id;
-  const [invoice, customers] = await Promise.all([
-    fetchInvoiceById(id),
-    fetchCustomers(),
-  ]);
+  
+  // const id= params.id;
+  // const [invoice, customers] = await Promise.all([
+  //   fetchInvoiceById(id),
+  //   fetchCustomers(),
+  // ]);
 
-  if(!invoice){
-    notFound();
+  // if(!invoice){
+  //   notFound();
   }
     return (
     <main>
